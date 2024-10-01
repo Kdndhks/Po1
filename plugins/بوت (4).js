da94118d98ddffca7cdceebe153763d3e5803fe9
Kdndhks <raiyahya31@gmail.com>
2024-10-01 21:26:06 +0300

    Update بوت (4).js

--- plugins/بوت (4).js
+++ plugins/بوت (4).js
@@ -23,7 +23,7 @@ let handler = async (m, {
 handler.help = ["gptvoc"]
 handler.tags = ["ai"];
 handler.command = /^(gptvoc)$/i
-handler.register = handler.limit = true
+handler.register = handler.limit = false
 
 export default handler
 
